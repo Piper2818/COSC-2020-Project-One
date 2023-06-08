4fb4e233145286a73608c840bc2e9ec869961e30
Piper2818 <136010132+Piper2818@users.noreply.github.com>
2023-06-08 14:46:01 -0600

    Add files via upload

---
+++ Node.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef NODE_
+#define NODE_
+
+template<class T> 
+
+class node
+{
+public: 
+	T data; 
+	node<T>* next; 
+	node()
+	{
+		next = nullptr;
+	}
+	node(T item)
+	{
+		data = item; 
+		next = nullptr; 
+	}
+
+};
+#endif; 
